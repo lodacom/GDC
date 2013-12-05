4c5ab38f6b761231a5c7952a1b13586fa4b11367
Duplouy <loda-compagnie@hotmail.fr>
2013-12-05 15:34:56 +0100

    Erreurs résolues
    Mais le marqueur ne s'ajoute pas

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -14,56 +14,49 @@ import views.html.*;
 public class Application extends Controller {
 
 	static Form<SearchForm> formulaire=Form.form(SearchForm.class);
-	
-    public static Result index() {
-    	GlobalRequest gr=new GlobalRequest();
-    	List<String> regions=gr.regions();
-    	List<String> departements=gr.departements();
-    	List<String> villes=gr.cities();
-    	
-    	List<TownInformation> infoRegions=null;
-        return ok(index.render(regions,departements,villes,infoRegions));
-    }
+	static List<TownInformation> infoRegions=null;
 
-    public static Result affichage(){
-    	GlobalRequest gr=new GlobalRequest();
-    	List<String> regions=gr.regions();
-    	List<String> departements=gr.departements();
-    	List<String> villes=gr.cities();
-    	
-    	List<TownInformation> infoRegions=null;
-        return ok(index.render(regions,departements,villes,infoRegions));
-    }
-    
-    public static Result search(){
-    	GlobalRequest gr=new GlobalRequest();
-    	List<String> regions=gr.regions();
-    	List<String> departements=gr.departements();
-    	List<String> villes=gr.cities();
-    	
-    	List<TownInformation> infoRegions=null;
-    	
-    	Form<SearchForm> filledForm = formulaire.bindFromRequest();
-    	if(filledForm.hasErrors()) {
-    		return ok(index.render(regions,departements,villes,infoRegions));
-    	}else{
-    		String region=filledForm.field("regions").value();
-    		String departement=filledForm.field("departements").value();
-    		String ville=filledForm.field("villes").value();
-    		String radio=filledForm.field("radio_zone").value();
-    		switch(radio){
-    		case "radio_region":
-    			infoRegions=gr.regions(region);
-    			break;
-    		case "radio_departement":
-    			
-    			break;
-    		case "radio_ville":
-    			
-    			break;
-    		}
-    		//System.out.println(region+" "+departement+" "+ville+" "+radio);
-    		return ok(Json.toJson(infoRegions));
-    	}
-    }
+	public static Result index() {
+		GlobalRequest gr=new GlobalRequest();
+		List<String> regions=gr.regions();
+		List<String> departements=gr.departements();
+		List<String> villes=gr.cities();
+
+		List<TownInformation> infoRegions=null;
+		return ok(index.render(regions,departements,villes,infoRegions));
+	}
+
+	public static Result affichage(){
+		GlobalRequest gr=new GlobalRequest();
+		List<String> regions=gr.regions();
+		List<String> departements=gr.departements();
+		List<String> villes=gr.cities();
+
+		Form<SearchForm> filledForm = formulaire.bindFromRequest();
+		if(filledForm.hasErrors()) {
+			return ok(index.render(regions,departements,villes,infoRegions));
+		}else{
+			String region=filledForm.field("regions").value();
+			String departement=filledForm.field("departements").value();
+			String ville=filledForm.field("villes").value();
+			String radio=filledForm.field("radio_zone").value();
+			switch(radio){
+			case "radio_region":
+				infoRegions=gr.regions(region);
+				break;
+			case "radio_departement":
+
+				break;
+			case "radio_ville":
+
+				break;
+			}
+			return ok(index.render(regions,departements,villes,infoRegions));
+		}
+	}
+
+
+	public static Result search(){
+		return ok(Json.toJson(infoRegions));
+	}
 }
