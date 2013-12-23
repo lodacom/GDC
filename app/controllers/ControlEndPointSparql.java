4cbe9e2b346861e93c613d0d7a04d87ce332ec53
Duplouy <loda-compagnie@hotmail.fr>
2013-12-23 18:58:30 +0100

    Début de mise en place du EndPoint perso

---
+++ app/controllers/ControlEndPointSparql.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.*;
+
+public class ControlEndPointSparql extends Controller {
+
+	public static Result index(){
+		return ok(endpoint_sparql.render());
+	}
+}
